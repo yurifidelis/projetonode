03137466c015056d6222694c840e87a978b69389
yurifidelis <129562565+yurifidelis@users.noreply.github.com>
2024-07-18 23:23:29 +0000

    att1

---
+++ projetonode/bibliot/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/', function(req, res, next) {
+  res.send("<h2>Sobre rotas....</h2>");});
+;
+
+
+
+
+module.exports = router;
