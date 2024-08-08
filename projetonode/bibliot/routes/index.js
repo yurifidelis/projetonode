a61f55a7a0b887af79cde9f54637f21ff391b837
yurifidelis <129562565+yurifidelis@users.noreply.github.com>
2024-08-08 23:57:51 +0000

    sdsdf2

--- projetonode/bibliot/routes/index.js
+++ projetonode/bibliot/routes/index.js
@@ -7,53 +7,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Procópio da Rede' });
 });
 
-router.get('/sobre', function(req, res) {
-  res.send('<h2>Sobre rotas</h2>');
+router.get('/autores/listar', function(req, res){
+  let cmd = 'SELECT idAutor, NoAutor, NoNacionalidade';
+      cmd += ' FROM TbAutor AS a INNER JOIN TbNacionalidade';
+      cmd += 'ON a.IdNacionalidade = n.IdNacionalidade';
+      cmd += 'ORDER BY NoAutor'; 
+db.query(cmd, [], function(erro, listagem)){
+  if (erro){
+    res.send(erro);
+  }
+  res.render('autores-lista',{resultado:listagem})
+}
 });
-
-
-router.get('/ola/:nome', function(req, res) {
-  res.send('<h2>Ola,' + req.params.nome + "</h2>");
-});
-
-router.get('/imc', function(req, res) {
-  let peso = req.query.peso;
-  let estatura = req.query.estatura
-
-  let imc = peso / Math.pow(estatura,2);
-  let msg = "<h3> seu imc é:" + imc.toFixed(2) +  "</h3>";
-  res.send(msg);
-});
-
-  let autores = ["miriam leitão",'ana beatriz'];
-  router.use(express.urlencoded({extended: true}));
-
-  router.get('/autores', function(req, res) {
-    res.json(autores);
-  });
-  router.get('/autores/consulta/:id', function(req,res){
-  res.json(autores)
-});
-
-router.get('/autores/consulta/:id', function(req,res) {
-  let id = req.params.id;
-  res.json(autores[id]);
-});
-router.post('/autores/inclui', function(req,res) {
-  let nome = req.body.nome;
-  autores.push(nome);
-  res.json(autores);
-});
-
-router.post('/autores/altera', function(req,res) {
-  let id = req.body.params.id;
-  let nome = req.body.nome;
-  
-  autores.splice(id,1);
-  res.json(autores);
-});
-
-router.get('/autores/add', function(req,res) {
+    
+  router.get('/autores/add', function(req,res) {
 
   res.render('autores-lista');
 });
